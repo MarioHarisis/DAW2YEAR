f2c78d216e46b1c6c17efcfce43005a23ebaf59e
Mario Harisis Garrido <147819942+mariothegreek@users.noreply.github.com>
2025-02-21 03:00:34 +0100

    Filtros de búsqueda
    Búsqueda por categoría y por marca

--- DWE Cliente/JS/Evaluable2_Cliente/js/script.js
+++ DWE Cliente/JS/Evaluable2_Cliente/js/script.js
@@ -1,13 +1,12 @@
 const tBody = document.querySelector(".tbody"); // seleccionar body de tabla carrito
-const cardContainer = document.querySelector("#card-container");
-const filtros = document.querySelectorAll("#filtro");
 const buscador = document.querySelector("#buscador");
 const mensajeContainer = document.querySelector("#mensaje-container");
 
 const url = "https://dummyjson.com/products";
 let productos = [];
 let listaCarrito = [];
-let listaFiltrada = [];
+let listaCategoria = []; //Set almacena valores únicos automáticamente
+let listaMarca = [];
 
 // Ejecutar la consulta y mostrar productos al cargar la página
 document.addEventListener("DOMContentLoaded", () => {
@@ -20,6 +19,15 @@ async function consultaProductos() {
   let respuestaJson = await respuesta.json();
 
   respuestaJson.products.forEach((element) => {
+    // crear lista de categorias sin repetidos
+    if (!listaCategoria.includes(element.category)) {
+      listaCategoria.push(element.category);
+    }
+
+    // crear lista de marcas sin repetidos
+    if (!listaMarca.includes(element.brand)) {
+      listaMarca.push(element.brand);
+    }
     // se crean los productos con sus atributos
     const producto = new Producto(
       element.id,
@@ -35,11 +43,13 @@ async function consultaProductos() {
 
     productos.push(producto);
   });
-
   mostrarProductos(productos);
+  crearFiltros(listaCategoria, "categoria");
+  crearFiltros(listaMarca, "marca");
 }
 
 function mostrarProductos(lista) {
+  const cardContainer = document.querySelector("#card-container");
   cardContainer.innerHTML = "";
   lista.forEach((producto) => {
     cardContainer.appendChild(crearCartaProducto(producto)); //añadir cada carta al main
@@ -75,7 +85,9 @@ function crearCartaProducto(producto) {
       </p>
     </div>
     <button type="button" class="btn btn-secondary btn-carrito">
-          Añadir al carrito
+          Añadir al carrito<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-fill" viewBox="0 0 16 16">
+  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4z"/>
+</svg>
         </button>
   `;
 
@@ -132,7 +144,7 @@ function crearCartaProducto(producto) {
   return nuevoProducto; // devolver el html que se insertará en el cardContainer;
 }
 
-// evento boton Comprar de carrito
+// boton "comprar" del carrito
 const btnComprar = document.querySelector(".animated-button");
 btnComprar.addEventListener("click", () => {
   if (listaCarrito.length == 0) {
@@ -155,9 +167,141 @@ btnComprar.addEventListener("click", () => {
   }).then((result) => {
     if (result.isConfirmed) {
       Swal.fire({
-        title: "¡Hecho!",
-        text: "Tu pedido llegará muy pronto.",
+        title: "Tu pedido llegará muy pronto.",
         icon: "success",
+        html: `
+        <div class="swal-container">
+        <div class="loader">
+  <div class="truckWrapper">
+    <div class="truckBody">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 198 93"
+        class="trucksvg"
+      >
+        <path
+          stroke-width="3"
+          stroke="#282828"
+          fill="#F83D3D"
+          d="M135 22.5H177.264C178.295 22.5 179.22 23.133 179.594 24.0939L192.33 56.8443C192.442 57.1332 192.5 57.4404 192.5 57.7504V89C192.5 90.3807 191.381 91.5 190 91.5H135C133.619 91.5 132.5 90.3807 132.5 89V25C132.5 23.6193 133.619 22.5 135 22.5Z"
+        ></path>
+        <path
+          stroke-width="3"
+          stroke="#282828"
+          fill="#7D7C7C"
+          d="M146 33.5H181.741C182.779 33.5 183.709 34.1415 184.078 35.112L190.538 52.112C191.16 53.748 189.951 55.5 188.201 55.5H146C144.619 55.5 143.5 54.3807 143.5 53V36C143.5 34.6193 144.619 33.5 146 33.5Z"
+        ></path>
+        <path
+          stroke-width="2"
+          stroke="#282828"
+          fill="#282828"
+          d="M150 65C150 65.39 149.763 65.8656 149.127 66.2893C148.499 66.7083 147.573 67 146.5 67C145.427 67 144.501 66.7083 143.873 66.2893C143.237 65.8656 143 65.39 143 65C143 64.61 143.237 64.1344 143.873 63.7107C144.501 63.2917 145.427 63 146.5 63C147.573 63 148.499 63.2917 149.127 63.7107C149.763 64.1344 150 64.61 150 65Z"
+        ></path>
+        <rect
+          stroke-width="2"
+          stroke="#282828"
+          fill="#FFFCAB"
+          rx="1"
+          height="7"
+          width="5"
+          y="63"
+          x="187"
+        ></rect>
+        <rect
+          stroke-width="2"
+          stroke="#282828"
+          fill="#282828"
+          rx="1"
+          height="11"
+          width="4"
+          y="81"
+          x="193"
+        ></rect>
+        <rect
+          stroke-width="3"
+          stroke="#282828"
+          fill="#DFDFDF"
+          rx="2.5"
+          height="90"
+          width="121"
+          y="1.5"
+          x="6.5"
+        ></rect>
+        <rect
+          stroke-width="2"
+          stroke="#282828"
+          fill="#DFDFDF"
+          rx="2"
+          height="4"
+          width="6"
+          y="84"
+          x="1"
+        ></rect>
+      </svg>
+    </div>
+    <div class="truckTires">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 30 30"
+        class="tiresvg"
+      >
+        <circle
+          stroke-width="3"
+          stroke="#282828"
+          fill="#282828"
+          r="13.5"
+          cy="15"
+          cx="15"
+        ></circle>
+        <circle fill="#DFDFDF" r="7" cy="15" cx="15"></circle>
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 30 30"
+        class="tiresvg"
+      >
+        <circle
+          stroke-width="3"
+          stroke="#282828"
+          fill="#282828"
+          r="13.5"
+          cy="15"
+          cx="15"
+        ></circle>
+        <circle fill="#DFDFDF" r="7" cy="15" cx="15"></circle>
+      </svg>
+    </div>
+    <div class="road"></div>
+
+    <svg
+      xml:space="preserve"
+      viewBox="0 0 453.459 453.459"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      xmlns="http://www.w3.org/2000/svg"
+      id="Capa_1"
+      version="1.1"
+      fill="#000000"
+      class="lampPost"
+    >
+      <path
+        d="M252.882,0c-37.781,0-68.686,29.953-70.245,67.358h-6.917v8.954c-26.109,2.163-45.463,10.011-45.463,19.366h9.993
+c-1.65,5.146-2.507,10.54-2.507,16.017c0,28.956,23.558,52.514,52.514,52.514c28.956,0,52.514-23.558,52.514-52.514
+c0-5.478-0.856-10.872-2.506-16.017h9.992c0-9.354-19.352-17.204-45.463-19.366v-8.954h-6.149C200.189,38.779,223.924,16,252.882,16
+c29.952,0,54.32,24.368,54.32,54.32c0,28.774-11.078,37.009-25.105,47.437c-17.444,12.968-37.216,27.667-37.216,78.884v113.914
+h-0.797c-5.068,0-9.174,4.108-9.174,9.177c0,2.844,1.293,5.383,3.321,7.066c-3.432,27.933-26.851,95.744-8.226,115.459v11.202h45.75
+v-11.202c18.625-19.715-4.794-87.527-8.227-115.459c2.029-1.683,3.322-4.223,3.322-7.066c0-5.068-4.107-9.177-9.176-9.177h-0.795
+V196.641c0-43.174,14.942-54.283,30.762-66.043c14.793-10.997,31.559-23.461,31.559-60.277C323.202,31.545,291.656,0,252.882,0z
+M232.77,111.694c0,23.442-19.071,42.514-42.514,42.514c-23.442,0-42.514-19.072-42.514-42.514c0-5.531,1.078-10.957,3.141-16.017
+h78.747C231.693,100.736,232.77,106.162,232.77,111.694z"
+      ></path>
+    </svg>
+  </div>
+</div>
+</div>
+        `,
       });
 
       // reducir stock de rpodcutos comprados
@@ -178,8 +322,11 @@ btnComprar.addEventListener("click", () => {
       tBody.innerHTML = ``;
     }
   });
+
+  /* TO DO: factura pase a ser lista de pedidos, que se puedan ver todos los pedidos que se hayan realizado */
 });
 
+// botón "eliminar" del carrito
 tBody.addEventListener("click", (e) => {
   if (e.target.classList.contains("btn-eliminar")) {
     // verificar si el botón pulsado es un eliminar
@@ -222,4 +369,95 @@ function comprobarStock(producto) {
   } else {
     stockText.textContent = " ";
   }
+}
+
+// Función para mostrar las alert
+function mostrarAlert(option, text) {
+  if (option == "success") {
+    return Swal.fire({
+      icon: "success",
+      title: `${text}`,
+      showConfirmButton: false,
+      timer: 1200,
+    });
+  } else if (option == "error") {
+    return Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: `${text}`,
+      confirmButtonColor: "#415A77",
+    });
+  }
+}
+
+// creación de filtros en nav
+const navbarNav = document.querySelector(".navbar-nav");
+function crearFiltros(lista, tipo) {
+  const navItem = document.createElement("li");
+  navItem.classList.add("nav-item", "dropdown", "ms-5");
+  navItem.innerHTML = `
+  <select
+    class="form-select ${tipo}"
+    id="filtro"
+    aria-label="Default select example"
+  >
+    <option selected hidden value="0">${tipo}</option>
+    </select>
+    `;
+  const select = navItem.querySelector(`.${tipo}`);
+  lista.forEach((element) => {
+    select.appendChild(createOption(element, element));
+  });
+  navbarNav.appendChild(navItem);
+}
+
+// editar dropdown de marca según categoría seleccionada
+navbarNav.addEventListener("click", (e) => {
+  const categoriaSelected = listaCategoria.find(
+    (cat) => cat == e.target.value // encontrar la categoria que ha sido seleccionada
+  );
+
+  if (categoriaSelected) {
+    console.log(categoriaSelected);
+
+    let prodEnCategoria = productos.filter(
+      (prod) => prod.categoria == categoriaSelected // crear lista con productos que tengan esta categoria
+    );
+
+    // cambiar las marcas que aparecen dependiendo de la categoría seleccionada
+    const select = navbarNav.querySelector(".marca");
+    select.innerHTML = ``;
+    select.appendChild(createOption(0, "Todos")); // primera opción nula
+
+    prodEnCategoria.forEach((element) => {
+      console.log(element);
+      // crear opción para cada marca y agregarlo al select
+      select.appendChild(createOption(element.marca, element.marca));
+    });
+
+    // listener botón filtros
+    document.querySelector(".btn-filtros").addEventListener("click", () => {
+      const marcaSeleccionada = select.value; // ver la marca que se ha seleccionado
+      mensajeContainer.textContent = `Mostrando productos: ${marcaSeleccionada}`;
+
+      if (marcaSeleccionada != 0) {
+        // mostrar solo productos con la marca seleccionada
+        let prodConMarca = prodEnCategoria.filter(
+          (prod) => prod.marca == marcaSeleccionada
+        );
+        mostrarProductos(prodConMarca);
+      } else {
+        //mostrar todos si no se ha seleccionado marca
+        mostrarProductos(prodEnCategoria);
+      }
+    });
+  }
+});
+
+// crear opcion para select
+function createOption(value, textContent) {
+  const option = document.createElement("option");
+  option.value = value;
+  option.textContent = textContent;
+  return option;
 }
