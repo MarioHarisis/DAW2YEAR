92bf6d95565cc6cfcd0aa1adb707578a794bed86
Mario Harisis Garrido <147819942+MarioHarisis@users.noreply.github.com>
2025-04-01 20:41:03 +0200

    Second lesson(routing)

--- DWE Cliente/AngularDaw/teoria/src/app/app-routing.module.ts
+++ DWE Cliente/AngularDaw/teoria/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AsignaturasComponent } from './components/asignaturas/asignaturas.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
+import { ListadoComponent } from './components/listado/listado.component';
+import { ErrorComponent } from './components/error/error.component';
 
-const routes: Routes = [];
+// ESTE ES EL MODULO DE GESTIÓN DE RUTAS
+const routes: Routes = [
+  {path: '', redirectTo: 'asignaturas', pathMatch: 'full'}, // redirecciona si no se especifica ruta
+  {path: 'asignaturas', component: AsignaturasComponent},
+  // ruta padre 'tecnologias' con dos rutas hijas
+  {path: 'tecnologias', component: TecnologiasComponent, 
+    children:[
+      {path: 'angular', component: AsignaturasComponent},
+      {path: 'react', component: ListadoComponent}
+    ]},
+  {path: 'listado', component: ListadoComponent},
+  {path: '**', component: ErrorComponent} // ERROR DEBE SER LA ULTIMA SIEMPRE(se evaluan en cascada)
+
+
+/* 
+Ejemplo de ruta que espera parámetros
+  {path: 'asignaturas/:id', component: AsignaturasComponent}, 
+  */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- DWE Cliente/AngularDaw/teoria/src/app/app.component.ts
+++ DWE Cliente/AngularDaw/teoria/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  
   title = 'teoria';
   autor = 'Mario';
   seleccion!: string;
+
+
+  constructor(private router: Router){}
+
+  navegar() {
+  console.log("Navegando");
+  this.router.navigate([("listado")]);
+  /* this.router.navigate(["listado", 5]); */
+  }
+
 }
